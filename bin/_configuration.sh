8f3789a8e9e747843065229314aa96aa21130b9f
berkane mohamed <berkane251994@gmail.com>
2021-12-13 12:29:10 +0100

    initial du projet

---
+++ bin/_configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MYSQL_USER="julien"
+MYSQL_PASSWORD="aaaa"
+MYSQL_HOST="localhost"
+WORDPRESS_BDD="bonus_wordpress"
+
+WORDPRESS_URL="http://localhost/cours/sheherazade/wp-custom-install/public"
+
+WORDPRESS_SITE_NAME="Wordpress sheherazade demo"
+WORDPRESS_ADMIN_NAME="admin"
+WORDPRESS_ADMIN_PASSWORD="admin"
+WORDPRESS_ADMIN_EMAIL="admin@mail.com"
+
+# optionnal configurations
+WORDPRESS_FOLDER="wp"
+WORDPRESS_CONTENT_FOLDER="content"
+WORDPRESS_TABLE_PREFIX="wpsh_"
